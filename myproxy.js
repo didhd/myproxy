68cd31248704fdb3aaef99b2b9f89a2e2cc9da11
Sanghwa Na <sa.na@samsung.com>
2020-04-22 13:15:35 +0900

    Update myproxy.js

--- myproxy.js
+++ myproxy.js
@@ -1,6 +1,6 @@
 function FindProxyForURL(url, host) {
     // use proxy for specific domains
-    if (shExpMatch(host, "*.samsung.net|samsung.net"))
+    if (shExpMatch(host, "*.samsung.net|samsung.net|*.maapops.com"))
         return "SOCKS5 localhost:1350";
 
     // by default use no proxy
