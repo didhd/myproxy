ab47d801bb29778e58f04d7b32ca12b6aba893c1
Sanghwa Na <sa.na@samsung.com>
2020-09-24 20:18:13 +0900

    Update myproxy.js

--- myproxy.js
+++ myproxy.js
@@ -3,6 +3,8 @@ function FindProxyForURL(url, host) {
     if (shExpMatch(host, "*.samsung.net|samsung.net"))
         return "SOCKS5 localhost:1350";
 
+    if (shExpMatch(host, "flutter.dev")) return "PROXY 168.219.61.215:8080";
+    
     // by default use no proxy
     return "DIRECT";
 }
