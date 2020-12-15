7b067f0f2d8d2ac33f3620f68316f61983fdbfe0
Sanghwa Na <sa.na@samsung.com>
2020-12-15 10:29:39 +0900

    Update myproxy.js

--- myproxy.js
+++ myproxy.js
@@ -1,4 +1,8 @@
 function FindProxyForURL(url, host) {
+    // no-proxy
+    if (shExpMatch(host, "github-action-stg.sec.samsung.net"))
+        return "DIRECT";
+    
     // use proxy for specific domains
     if (shExpMatch(host, "*.samsung.net|samsung.net"))
         return "SOCKS5 localhost:1350";
